71abd3d9ec9bd05ccf01a212ffebad8519ac6c81
Hilmi AM <imiia75775@proton.me>
2024-01-02 17:35:29 +0700

    Initial commit

---
+++ src/main/java/franxxcode/servlet/JavaWebServletApplication.java
@@ -0,0 +1,13 @@
+package franxxcode.servlet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaWebServletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaWebServletApplication.class, args);
+	}
+
+}
---
+++ src/test/java/franxxcode/servlet/JavaWebServletApplicationTests.java
@@ -0,0 +1,13 @@
+package franxxcode.servlet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaWebServletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
