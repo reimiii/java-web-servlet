fdfaae31ceb820342d60bc846bdd6034cea6d87a
Hilmi AM <imiia75775@proton.me>
2024-01-03 13:00:14 +0700

    race condition, servlet is singleton

---
+++ src/main/java/franxxcode/servlet/CounterServlet.java
@@ -0,0 +1,25 @@
+package franxxcode.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.concurrent.atomic.AtomicLong;
+
+@WebServlet(urlPatterns = "/counter")
+public class CounterServlet extends HttpServlet {
+
+    private final AtomicLong atomicLong = new AtomicLong(0);
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        long get = atomicLong.incrementAndGet();
+        resp.getWriter().println(
+                "Total Count: " + get
+        );
+    }
+}
