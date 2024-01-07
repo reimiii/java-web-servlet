b057e91a5f1585785733e10c24d4d4eae15bbce3
Hilmi AM <imiia75775@proton.me>
2024-01-07 16:39:01 +0700

    Session

---
+++ src/main/java/franxxcode/servlet/SessionServlet.java
@@ -0,0 +1,26 @@
+package franxxcode.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/session")
+public class SessionServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession(true);
+        String username = (String) session.getAttribute("username");
+
+        if (username == null) {
+            resp.getWriter().println("U are not login");
+        } else {
+            resp.getWriter().println("Hello: " + username);
+        }
+    }
+}
